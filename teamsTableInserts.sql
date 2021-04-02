1e34747494c12ea6229fc497987e827eed776ab9
rschulte12 <41649489+rschulte12@users.noreply.github.com>
2021-04-02 15:38:59 -0500

    Create teamsTableInserts.sql

---
+++ teamsTableInserts.sql
@@ -0,0 +1,6 @@
+INSERT into teams(city, nickname)
+VALUES('Atlanta', 'Hawks'),('Boston', 'Celtics'),
+		('Brooklyn', 'Nets'),('Charlotte', 'Hornets'),
+        ('Chicago', 'Bulls'),('Cleveland', 'Cavaliers'),
+        ('Dallas', 'Mavericks'),('Denver', 'Nuggets'),
+        ('Detroit', 'Pistons'),('Golden State', 'Warriors');
