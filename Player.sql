ba05ee854cfe36b00b03f39a11730c4d525a2f5b
Bibek Pokharel <55300843+bibpok@users.noreply.github.com>
2021-04-02 23:23:32 -0500

    Add files via upload

---
+++ Player.sql
@@ -0,0 +1,130 @@
+INSERT INTO Players(fName, lName, teamID)
+VALUES
+/* Team For Atlanta Hawks  */
+('Begdan','Bagdanovic','A1'),
+('Clint','Capela','A1'),
+('Kris','Dunn','A1'),
+('Bruno','Fernando','A1'),
+('Brandon','Goodwin','A1'),
+('Solomon','Hill','A1'),
+('DeAndre','Hunter','A1'),
+('Nathan','Knight','A1'),
+('Onyeka','Okongwu','A1'),
+('Cam','Reddish','A1'),
+
+/* Team For Boston Celtics  */
+ ('Jaylen','Brown','	B1'),
+('Carsen','Edwards','B1'),
+('Tacko','Fall','B1'),
+('Evan','Fournier','B1'),
+('Luke','Kornet','B1'),
+('Romeo','Langford','B1'),
+('Aaron','Nesmith','B1'),
+('Semi','Ojeleye','B1'),
+('Payton','Pritchard','B1'),
+('Marcus','Smart','B1'),
+
+/* Team For Brooklyn Nets  */
+('Lamarcus','Aldridge','C1'),
+('Bruce','Brown', 'C1'),
+('Chris','Chiozza', 'C1'),
+('Nicolas','Claxton', 'C1'),
+('Kevin','Durant', 'C1'),
+('Jeff','Green', 'C1'),
+('Blake','Griffin', 'C1'),
+('James','Harden', 'C1'),
+('Kyrie','Irving', 'C1'),
+('Tyler','Johnson', 'C1'),
+
+/* Team For Charlotte Hornets  */
+('LaMelo','Ball','D1'),
+('Bismack','Biyombo','D1'),
+('Miles','Bridges','D1'),
+('Vernon','Jr','D1'),
+('Malik','Monk','D1'),
+('Nick','Richards','D1'),
+('Grant','Riller','D1'),
+('Terry','Rozier','D1'),
+('PJ','Washington','D1'),
+('Cody','Zeller','D1'),
+
+/* Team For Chicago Bulls */
+('Ryan','Arcidiacono','E1'),
+('Troy','Brown','E1'),
+('Devon','Dotson','E1'),
+('Cristiano','Felicio','E1'),
+('Javonte','Green','E1'),
+('Zach','Lavine','E1'),
+('Lauri','Markkenen','E1'),
+('Adam','Mokoka','E1'),
+('Tomas','Satoransky','E1'),
+
+
+/* Team For Clevland Cavaliers  */
+('Jarrett','Allen','F1'),
+('Matthew','Dellavedoda','F1'),
+('Damyean','Dotson','F1'),
+('Darius','Garland','F1'),
+('Kevin','Love','F1'),
+('Larry','Nance','F1'),
+('Isaac','Okoro','F1'),
+('Cedi','Osman','F1'),
+('Collin','Sexton','F1'),
+('Lamar','Stevens','F1'),
+
+
+/* Team Dallas Mavericks  */
+('Tyler','Bey','G1'),
+('Jalen','Brunsen','G1'),
+('Trey','Burke','G1'),
+('Luka','Doncic','G1'),
+('Josh','Green','G1'),
+('Tim','Hardaway','G1'),
+('Nate','Hinton','G1'),
+('Maxi','Kleber','G1'),
+('Boban','Marjanovic','G1'),
+('Kristapas','Porzingis','G1'),
+
+
+/* Team For Denver Nuggets  */
+('Will','Barton','H1'),
+('Bol','Bol','H1'),
+('Facundo','Campazzo','H1'),
+('Vlatko','Cancar','H1'),
+('PJ','Dozier','H1'),
+('Aaron','Gordon','H1'),
+('JaMychal','Green','H1'),
+('Markus','Howard','H1'),
+('JaVale','McGee','H1'),
+('Paul','Millsap','H1'),
+
+/* Team For Detroit Pistons  */
+('Saddiq','Bey','I1'),
+('Tyler','Cook','I1'),
+('Hamidou','Diallo','I1'),
+('Sekou','Doumbouya','I1'),
+('Jerami','Grant','I1'),
+('Killian','Hayes','I1'),
+('Frank','Jackson','I1'),
+('Josh','Jackson','I1'),
+('Sanen','Lee','I1'),
+('Rodney','McGruder','I1'),
+
+
+/*. Team for Golden State Warrior */
+('Kent','Bazemore','J1'),
+('Srephen','Curry','J1'),
+('Draymond','Green','J1'),
+('Damion','Lee','J1'),
+('Nico','Mannion','J1'),
+('Mychal','Mulder','J1'),
+('Kelly','Oubre','J1'),
+('Eric','Paschall','J1'),
+('Alen','Smailagic','J1'),
+('Klay','Thompson','J1');
+
+
+
+
+
+SELECT * FROM Players;
